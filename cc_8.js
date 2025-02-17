69d9b557c824e5b5eaa241fa4e71367ecd507cfc
Ashley Le <ashleyle@usf.edu>
2025-02-16 21:42:17 -0500

    Task 2 - Product Price After Discount

--- cc_8.js
+++ cc_8.js
@@ -6,3 +6,12 @@ function calculateSalary(baseSalary, bonus, taxRate) {
 
 console.log(calculateSalary(5000, 500, 0.1)); //"Net Salary: $5000.00"
 console.log(calculateSalary(7000, 1000, 0.15)); //"Net Salary: $6950.00"
+
+//Task 2: Function Expression
+function calculateDiscount(price, discountRate) {
+    let finalPrice = price - (price * discountRate); //formula to find final price
+    return `Final Price: $${finalPrice.toFixed(2)}`; //displaying final price
+};
+
+console.log(calculateDiscount(100, 0.2)); //"Final Price: $80.00"
+console.log(calculateDiscount(250, 0.15)); //"Final Price: $212.50"
